b0de19647ca731818986c6e4225a22784fe39521
abracadanis <greenslyme21@gmail.com>
2022-09-14 11:47:25 +0200

    Frontend TS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,36 @@ import './App.scss';
 
 import Navbar from "./components/Navbar";
 import ProjectList from "./components/ProjectList/ProjectList";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import projectItem from "./components/ProjectList/ProjectItem";
+import {Api, ApplicationApi, Configuration} from "./openapi";
+
+const configuration = new Configuration({
+    basePath: "http://localhost:8180/api/findev"
+})
+
+const api = new ApplicationApi(configuration)
 
 const App = (props: any) => {
+
+    useEffect(() => {
+        api.getUsers().then(data => {setUsers(data)})
+    }, [])
+
+    const [users, setUsers] = useState<Api.UserSo[]>([])
 
     const projects = [
         {"id": 123, "title": "title", "description": "description"},
         {"id": 321, "title": "title11", "description": "description11"},
     ]
 
+
+
   return (
       <div>
         <Navbar/>
         <ProjectList project={projects}/>
+          {JSON.stringify(users)}
       </div>
   );
 }
